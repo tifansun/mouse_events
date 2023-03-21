b462a62a3fcbf7b27ff2e3eadb6d0e2ba4c76227
Carmen Ortigoza <43401320+ZzalyOreo@users.noreply.github.com>
2023-03-21 14:58:42 -0600

    eventos en español

--- main.js
+++ main.js
@@ -5,15 +5,15 @@ evento_detectado = document.getElementById('evento_detectado');
 div.addEventListener("mousedown", function (e) {
     div.style.background = "#feb236";
     mouse_event = "clicked";
-    evento_detectado.innerHTML = "BAJAR EL MOUSE";
+    evento_detectado.innerHTML = "mouse down";
 });
 
 div.addEventListener("mousemove", function (e) {
     div.style.background = "#d64161#d64161";
-    evento_detectado.innerHTML = "MOVER EL MOUSE";
+    evento_detectado.innerHTML = "mause move";
 
     if (mouse_event == "clicked") {
-        evento_detectado.innerHTML = "BAJAR EL MOUSE <br>+<br> MOVER EL MOUSE";
+        evento_detectado.innerHTML = "mouse down <br>+<br> mouse move";
         div.style.background = "#ff7b25";
     }
 });
@@ -21,11 +21,11 @@ div.addEventListener("mousemove", function (e) {
 div.addEventListener("mouseup", function (e) {
     div.style.background = "#6b5b95";
     mouse_event = "mouseUp";
-    evento_detectado.innerHTML = "ALZAR EL MOUSE";
+    evento_detectado.innerHTML = "mouse up";
 });
 
 div.addEventListener("mouseleave", function (e) {
     div.style.background = "grey";
     mouse_event = "mouseDown";
-    evento_detectado.innerHTML = "RETIRAR EL MOUSE";
+    evento_detectado.innerHTML = "mouse leave";
 });
